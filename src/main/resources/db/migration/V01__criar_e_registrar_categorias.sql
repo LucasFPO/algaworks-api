2d3727af6b77b3aad7072786526aa82621b0ba3e
Lucas Oliveira <lucas.f.pedrosa@gmail.com>
2019-02-26 17:04:55 -0300

    Criando a migração e entidade de lançamento

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,3 +1,7 @@
+/* Lembrando que a partir do momento que se usa o Flyway, você não pode mais alterar a estrutura do banco
+de dados (criar tabelas, colunas...) manualmente direto no banco de dados (MySQL Workbench).
+Tudo que for alterar na estrutura do banco de dados, é necessário que seja feito a partir de migrations.*/
+
 CREATE TABLE categoria (
 	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50) NOT NULL
