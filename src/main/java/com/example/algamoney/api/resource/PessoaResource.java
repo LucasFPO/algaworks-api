8fe1ef9732923ff0521f1c52e0b47a52a9a7ab86
Lucas Oliveira <lucas.f.pedrosa@gmail.com>
2019-03-17 19:21:01 -0300

    Pesquisando pessoa de acordo com o nome

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -1,13 +1,12 @@
 package com.example.algamoney.api.resource;
 
-import java.net.URI;
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,15 +17,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.algamoney.api.event.RecursoCriadoEvent;
-import com.example.algamoney.api.model.Categoria;
 import com.example.algamoney.api.model.Pessoa;
 import com.example.algamoney.api.repository.PessoaRepository;
 import com.example.algamoney.api.service.PessoaService;
+
 
 @RestController
 @RequestMapping("/pessoas")
@@ -41,11 +40,11 @@ public class PessoaResource {
 	@Autowired // Dispara o evento
 	private ApplicationEventPublisher publisher;
 
-	@GetMapping // Sem esse bloco de código, eu não consigo usar "Listar Pessoas" no postman.
+	/* @GetMapping // Sem esse bloco de código, eu não consigo usar "Listar Pessoas" no postman.
 	// ELE É REPETITIVO, POSSO TAMBÉM CRIAR UM EVENT LISTENER SE QUISER
 	public List<Pessoa> listar() {
 		return pessoaRepository.findAll();
-	}
+	}*/
 	
 	@PostMapping                               // Request = Requisição   
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_PESSOA') and #oauth2.hasScope('write')")
@@ -101,5 +100,12 @@ public class PessoaResource {
 		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
 	}
 	
+	
+	// Criado para pesquisar pessoa, de acordo com o nome
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
+	}
 	
 }
